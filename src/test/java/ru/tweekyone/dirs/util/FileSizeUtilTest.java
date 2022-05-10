641fe62f6f24c88f1f252592c40804e43d15a252
Evgenii Ponukhov <tweeky92@gmail.com>
2022-05-10 13:56:39 +0300

    created FileSizeUtilTest

--- src/test/java/ru/tweekyone/dirs/util/FileSizeUtilTest.java
+++ src/test/java/ru/tweekyone/dirs/util/FileSizeUtilTest.java
@@ -1,12 +1,34 @@
 package ru.tweekyone.dirs.util;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static ru.tweekyone.dirs.util.FileSizeUtil.getReadableFileSize;
 
 class FileSizeUtilTest {
 
     @Test
-    void getReadableFileSize() {
+    void getByteFileSize() {
+        Assertions.assertThat(getReadableFileSize(1l)).isEqualTo("1 B");
+    }
+
+    @Test
+    void getKByteFileSize() {
+        Assertions.assertThat(getReadableFileSize(1024l)).isEqualTo("1 KB");
+    }
+
+    @Test
+    void getMByteFileSize() {
+        Assertions.assertThat(getReadableFileSize(1048600l)).isEqualTo("1 MB");
+    }
+
+    @Test
+    void getGByteFileSize() {
+        Assertions.assertThat(getReadableFileSize(1073741850l)).isEqualTo("1 GB");
+    }
+
+    @Test
+    void getTByteFileSize() {
+        Assertions.assertThat(getReadableFileSize(1099511627776l)).isEqualTo("1 TB");
     }
 }
